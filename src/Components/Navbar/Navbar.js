92d8c4f2dded2713eedc4f7c335340a8e07b12e2
playck <playck@naver.com>
2021-08-03 20:10:09 +0900

    [ADD] Add Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav>
+      <div className="company">DDIVE</div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Main from "./Pages/Main/Main";
 
 export default class Routes extends Component {
   render() {
     return (
       <Router>
+        <Navbar />
         <Switch>
-          <Route exact path="/" />
+          <Route exact path="/" component={Main} />
         </Switch>
       </Router>
     );
