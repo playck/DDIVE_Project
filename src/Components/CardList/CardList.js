9dff599402f56c5e972b0f07757fccf84b992f2a
playck <playck@naver.com>
2021-08-04 15:05:16 +0900

    [ADD] Add CardList infinityScroll

--- src/Components/CardList/CardList.js
+++ src/Components/CardList/CardList.js
@@ -1,37 +1,62 @@
 import axios from "axios";
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Card from "../Card/Card";
 import "./CardList.scss";
 
 const CardList = () => {
   const [cardList, setCardList] = useState(null);
+  const [perPage, setPerpage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const loader = useRef();
 
-  const getCardtData = () => {
+  const getCardtData = (perPage) => {
+    setIsLoading(true);
     axios
-      .get("https://reqres.in/api/users?per_page=12&page=1")
+      .get(`https://reqres.in/api/users?per_page=${perPage}&page=1`)
       .then(function (res) {
         setCardList(res.data);
-      });
+        setIsLoading(false);
+      })
+      .catch((error) => console.warn(error));
   };
 
   useEffect(() => {
-    getCardtData();
-  }, []);
+    perPage !== 0 && getCardtData(perPage);
+  }, [perPage]);
+
+  const onIntersect = (entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) setPerpage((perPage) => perPage + 12);
+  };
+
+  useEffect(() => {
+    if (!loader.current) return;
+
+    const io = new IntersectionObserver(onIntersect, { threshold: 1 });
+    io.observe(loader.current);
+
+    return () => io && io.disconnect();
+  }, [loader]);
 
   return (
-    <ul className="cardList">
-      {cardList &&
-        cardList.data.map((card) => (
-          <Card
-            id={card.id}
-            avatar={card.avatar}
-            firstName={card.first_name}
-            lastName={card.last_name}
-            email={card.email}
-          />
-        ))}
-    </ul>
+    <>
+      <ul className="cardList">
+        {cardList &&
+          cardList.data.map((card) => (
+            <Card
+              id={card.id}
+              avatar={card.avatar}
+              firstName={card.first_name}
+              lastName={card.last_name}
+              email={card.email}
+            />
+          ))}
+      </ul>
+      <div ref={loader} className="loader">
+        {isLoading && "Loading..."}
+      </div>
+    </>
   );
 };
 
